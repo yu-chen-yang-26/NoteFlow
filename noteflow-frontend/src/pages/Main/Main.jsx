72e2c34b1160566fcbb0b23e3092b7efc74ad1fe
r11725063 <tsailawrence812@gmail.com>
2023-05-12 15:21:02 +0800

    css update

--- noteflow-frontend/src/pages/Main/Main.jsx
+++ noteflow-frontend/src/pages/Main/Main.jsx
@@ -1,23 +1,23 @@
-import Sidebar from '../../Components/Sidebar/Sidebar.jsx';
+import Sidebar from "../../Components/Sidebar/Sidebar.jsx";
 
-import './Main.scss';
-import FlowGrid from '../../Components/FlowGrid/FlowGrid.jsx';
-import PageTab from '../../Components/PageTab/PageTab.jsx';
-import Library from '../../Components/Library/Library.jsx';
-import { useFlowStorage } from '../../storage/Storage';
-import Calendar from '../../Components/Calendar/Calendar.jsx';
-import Settings from '../../Components/Settings/Settings.jsx';
+import "./Main.scss";
+import FlowGrid from "../../Components/FlowGrid/FlowGrid.jsx";
+import PageTab from "../../Components/PageTab/PageTab.jsx";
+import Library from "../../Components/Library/Library.jsx";
+import { useFlowStorage } from "../../storage/Storage";
+import Calendar from "../../Components/Calendar/Calendar.jsx";
+import Settings from "../../Components/Settings/Settings.jsx";
 
 export default function Main() {
   const mode = useFlowStorage((state) => state.mode);
   console.log(mode);
   return (
-    <div className='App'>
-      <div className='App-container'>
+    <div className="App">
+      <div className="App-container">
         <Sidebar />
-        <div className='App-tab'>
+        <div className="App-tab">
           <PageTab />
-          <div className='Flow-grid'>
+          <div className="Flow-grid">
             {mode === 0 ? (
               <FlowGrid />
             ) : mode === 1 ? (
