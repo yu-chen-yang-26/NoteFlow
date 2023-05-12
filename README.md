e69b2aaa2ea6cd5ae2c0a41df281f9f53d65f99d
Yoho <vnaticzhock@gmail.com>
2023-05-12 14:23:02 +0800

    register

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 前後端開發
 ```bash
 docker compose --env-file .compose.env up -d
-
 ## frontend will be open on 7415 by default.
 ## but for same origin policy, you must not specify port when you want to access noteflow.
 ## you need to install and configure nginx for non-port support.
--- noteflow-backend/src/controller/userServices/register.js
+++ noteflow-backend/src/controller/userServices/register.js
@@ -2,77 +2,75 @@
 import { v4 as uuidv4 } from 'uuid';
 import _ from 'lodash';
 import argon2 from 'argon2';
+import crypto from 'crypto-js';
 import db from '../../lib/db.js';
 import sendEmail from '../../lib/email.js';
-import crypto from 'crypto-js';
 import HTML_TEMPLATE from '../../lib/mail-template.js';
 import { createUserBucket } from '../../model/mongodb/model/index.js';
 
-let { EMAIL_USER, EMAIL_HOST } = process.env;
+const { EMAIL_USER, EMAIL_HOST } = process.env;
 
 const register = async (ctx) => {
-    try {
-        const { user } = ctx.request.body; // if none, assign user with {}
+  try {
+    const { user } = ctx.request.body; // if none, assign user with {}
 
-        if (!user) {
-            ctx.throw(400, "Bad request. You didn't provide user column.");
-        }
-        if (!user.email || !user.name || !user.password) {
-            ctx.throw(
-                400,
-                "Bad request. You didn't provide sufficient information."
-            );
-        }
-
-        // create User
-        const result = await db('users').first().where({ email: user.email });
-        if (result) {
-            ctx.throw(401, 'Forbidden, you already have  an email.');
-        }
-
-        var userId = null;
-        var randomString = Math.random().toString(15);
-        var token = crypto.SHA256(randomString).toString(crypto.enc.Hex);
-        var verifyMessage = 'An Email sent to your account please verify';
-        var password = await argon2.hash(user.password);
-
-        const userValidate = await db('users')
-            .insert({
-                uuid: uuidv4(),
-                name: user.name,
-                password: password,
-                email: user.email,
-                token: token,
-            })
-            .returning('*')
-            .then((rows) => {
-                console.log(
-                    'User created successfully. New user id is:',
-                    rows[0].id
-                );
-                userId = rows[0].id;
-            });
-
-        const message = `${EMAIL_HOST}/api/user/verify/${userId}/${token}`;
-        // await sendEmail(user.email, 'Verify Email', verifyMessage);
-
-        sendEmail({
-            subject: '【noteflow】 Verify Email',
-            text: message,
-            to: user.email,
-            from: EMAIL_USER,
-            html: HTML_TEMPLATE(message),
-        });
-
-        await createUserBucket(user.email);
-
-        ctx.status = 200;
-        ctx.body = verifyMessage;
-    } catch (err) {
-        console.log(err);
-        ctx.status = err.status || 500;
-        ctx.body = JSON.stringify({ errors: err.message });
+    if (!user) {
+      ctx.throw(400, "Bad request. You didn't provide user column.");
     }
+    if (!user.email || !user.name || !user.password) {
+      ctx.throw(400, "Bad request. You didn't provide sufficient information.");
+    }
+
+    // create User
+    const result = await db('users').first().where({ email: user.email });
+    if (result) {
+      ctx.throw(401, 'Forbidden, you already have  an email.');
+    }
+
+    let userId = null;
+    const randomString = Math.random().toString(15);
+    const token = crypto.SHA256(randomString).toString(crypto.enc.Hex);
+    const verifyMessage = 'An Email sent to your account please verify';
+    const password = await argon2.hash(user.password);
+
+    const userValidate = await db('users')
+      .insert({
+        uuid: uuidv4(),
+        name: user.name,
+        password,
+        email: user.email,
+        token,
+      })
+      .returning('*')
+      .then((rows) => {
+        console.log('User created successfully. New user id is:', rows[0].id);
+        userId = rows[0].id;
+      });
+
+    const message = `${EMAIL_HOST}/api/user/verify/${userId}/${token}`;
+    // await sendEmail(user.email, 'Verify Email', verifyMessage);
+
+    sendEmail({
+      subject: '【noteflow】 Verify Email',
+      text: message,
+      to: user.email,
+      from: EMAIL_USER,
+      html: HTML_TEMPLATE(message),
+    });
+
+    ctx.session.logined = true;
+    ctx.session.email = user.email;
+    ctx.session.name = user.name;
+    ctx.session.picture = user.picture;
+
+    await createUserBucket(user.email);
+
+    ctx.status = 200;
+    ctx.body = verifyMessage;
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = JSON.stringify({ errors: err.message });
+  }
 };
 
 export default register;
