5ed84be7843b2f4189c66bbdb441a2148aa072ac
Yoho <vnaticzhock@gmail.com>
2023-05-12 20:57:29 +0800

    hi

--- noteflow-frontend/src/API/api.js
+++ noteflow-frontend/src/API/api.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 let BASE_URL = import.meta.env.VITE_NOTEFLOW_BACKEND;
 
-if (import.meta.env.VITE_DEV === "1") {
+if (import.meta.env.VITE_DEV === '1') {
   BASE_URL = `localhost`;
 }
 
