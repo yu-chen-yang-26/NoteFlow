44b1f10bb61d11b9a5b39d242f4820a158ff9be4
oohyuti <yuti0622@gmail.com>
2023-05-27 01:47:04 +0800

    add button when clicking a node

--- noteflow-frontend/src/Components/FlowGrid/FlowGrid.jsx
+++ noteflow-frontend/src/Components/FlowGrid/FlowGrid.jsx
@@ -302,7 +302,7 @@ export default function FlowGrid({ containerRef }) {
                   <Menu
                     // autoFocusItem={open}
                     open={isMenuOpen == flow.id}
-                    // anchorEl={target}
+                    anchorEl={target}
                     anchorOrigin={{
                       vertical: 'center',
                       horizontal: 'center',
