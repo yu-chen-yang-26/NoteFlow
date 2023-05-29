20f189764a7dc1436a7f48d99f3938a10e2b1f59
Yoho <vnaticzhock@gmail.com>
2023-05-30 02:20:24 +0800

    hi

--- noteflow-backend/src/controller/userServices/getUserPhoto.js
+++ noteflow-backend/src/controller/userServices/getUserPhoto.js
@@ -9,9 +9,9 @@ const getUserPhoto = async (ctx) => {
   if (!email) {
     email = ctx.session.email;
   }
-<<<<<<< HEAD
+  let result;
   try {
-    const result = await db('users').first().where({ email });
+    result = await db('users').first().where({ email });
     if (result.picture) {
       if (
         !result.picture.startsWith('http') &&
@@ -21,18 +21,11 @@ const getUserPhoto = async (ctx) => {
         db('users').insert({ picture: null }).where({ email });
       }
     }
-=======
->>>>>>> dc9ba0462887d76f20f8c9f7219d74c3f4be56f5
-
-  let result;
-  try {
-    result = await db('users').first().where({ email });
   } catch (e) {}
 
   ctx.status = CODE.success;
   ctx.body = JSON.stringify(result ? result.picture : null);
-  ctx.set('Content-Type', 'application/json')
-  
+  ctx.set('Content-Type', 'application/json');
 };
 
 export default getUserPhoto;
