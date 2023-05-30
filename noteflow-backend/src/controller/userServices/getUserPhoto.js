af9a79ddf22b1cd15ed71660926f46c2edafaebe
RyLai <ryanlai880122@gmail.com>
2023-05-30 14:02:44 +0800

    sync

--- noteflow-backend/src/controller/userServices/getUserPhoto.js
+++ noteflow-backend/src/controller/userServices/getUserPhoto.js
@@ -9,20 +9,6 @@ const getUserPhoto = async (ctx) => {
   if (!email) {
     email = ctx.session.email;
   }
-<<<<<<< HEAD
-  try {
-    const result = await db('users').first().where({ email });
-    if (result.picture) {
-      if (
-        !result.picture.startsWith('http') &&
-        !fs.existsSync(path.join(process.cwd(), 'images', result.picture))
-      ) {
-        result.picture = null;
-        db('users').insert({ picture: null }).where({ email });
-      }
-    }
-=======
->>>>>>> dc9ba0462887d76f20f8c9f7219d74c3f4be56f5
 
   let result;
   try {
@@ -31,8 +17,7 @@ const getUserPhoto = async (ctx) => {
 
   ctx.status = CODE.success;
   ctx.body = JSON.stringify(result ? result.picture : null);
-  ctx.set('Content-Type', 'application/json')
-  
+  ctx.set('Content-Type', 'application/json');
 };
 
 export default getUserPhoto;
--- noteflow-frontend/src/API/api.js
+++ noteflow-frontend/src/API/api.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 let BASE_URL = 'noteflow.live';
 
-// if (import.meta.env.VITE_DEV === '1') {
-//   BASE_URL = `localhost`;
-// }
+if (import.meta.env.VITE_DEV === '1') {
+  BASE_URL = `localhost`;
+}
 
 console.log('bundle with route:', BASE_URL);
 
 const instance = axios.create({
-  baseURL: `https://${BASE_URL}/api`,
+  baseURL: `http://${BASE_URL}/api`,
   withCredentials: true,
 });
 
